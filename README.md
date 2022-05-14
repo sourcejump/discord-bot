3c0055c50c6e0740af6b4b9970125e730696fada
Eric <46975257+ecsr@users.noreply.github.com>
2022-05-14 21:30:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Discord Bot
+A Discord bot for the [SourceJump](https://sourcejump.net) community built with [discord.js](https://github.com/discordjs/discord.js).
+
+## Contributing
+
+1. Fork the [git repository](https://github.com/sourcejump/discord-bot).
+2. Create a feature branch `git checkout -b new-feature`
+3. Make your changes.
+4. Add, commit and push your changes.
+5. Submit a pull request.
---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, Collection, Intents } = require('discord.js');
+
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Load commands from command files.
+client.commands = new Collection();
+const commands = [];
+const commandsPath = path.join(__dirname, 'src', 'commands');
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+}
+
+// Register commands with Discord's API.
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+    try {
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+            body: commands,
+        });
+        console.log('Successfully registered commands!');
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true,
+        });
+        console.error(error);
+    }
+});
+
+client.login(token);
