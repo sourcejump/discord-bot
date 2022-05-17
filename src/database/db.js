42d2b3f721952f8c8ddaf7dad8e068d3b6d55339
CodeKhenzy <codingkhenzy@gmail.com>
2022-05-17 22:39:39 +0200

    removed port from typeormconnection in db.js

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,6 @@ const typeorm = require('typeorm');
 const typeormConnection = new typeorm.DataSource({
     type: 'mysql',
     url: process.env.DATABASE_URL,
-    port: process.env.SQL_PORT,
     synchronize: true, //disable when in production,
     entities: [], //require('./path/to/entity'), ...
 });
