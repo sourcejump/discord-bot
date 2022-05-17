0b5ee50eff61f8303fec2896caabcd2bf2584c1b
CodeKhenzy <codingkhenzy@gmail.com>
2022-05-17 22:33:02 +0200

    changed to connection string

--- src/database/db.js
+++ src/database/db.js
@@ -2,11 +2,8 @@ const typeorm = require('typeorm');
 
 const typeormConnection = new typeorm.DataSource({
     type: 'mysql',
-    host: process.env.SQL_HOST,
-    username: process.env.SQL_USER,
-    database: process.env.SQL_DATABASE,
+    url: process.env.DATABASE_URL,
     port: process.env.SQL_PORT,
-    password: process.env.SQL_PW,
     synchronize: true, //disable when in production,
     entities: [], //require('./path/to/entity'), ...
 });
