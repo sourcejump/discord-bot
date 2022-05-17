df0d5c224c425154d4b81a322a914164a1cbd8b6
CodeKhenzy <codingkhenzy@gmail.com>
2022-05-17 22:09:19 +0200

    added initial typeorm integration

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,14 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const typeorm = require('typeorm');
 
-const poolConnection = mysql.createPool({
+const typeormConnection = new typeorm.DataSource({
+    type: 'mysql',
     host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
+    username: process.env.SQL_USER,
     database: process.env.SQL_DATABASE,
     port: process.env.SQL_PORT,
     password: process.env.SQL_PW,
-    waitForConnections: true,
-    connectionLimit: 20,
-    queueLimit: 0,
+    synchronize: true, //disable when in production,
+    entities: [], //require('./path/to/entity'), ...
 });
 
-module.exports = poolConnection;
+module.exports = typeormConnection;
