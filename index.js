61f56801a2d64ee2e71b5b4b2b7d0baee6e3d448
zeltim1 <tim.guenzel5@web.de>
2022-05-20 17:39:45 +0200

    required discord-modals and set modal(client)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { Client, Collection, Intents } = require('discord.js');
+const modal = require("discord-modals");
 
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
@@ -12,6 +13,8 @@ const guildId = process.env.GUILD_ID;
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+
+modal(client);
 
 // Load events from event files.
 const eventsPath = path.join(__dirname, 'src', 'events');
@@ -75,7 +78,7 @@ client.on('interactionCreate', async (interaction) => {
     }
 
     try {
-        await command.execute(interaction);
+        await command.execute(interaction, modal);
     } catch (error) {
         await interaction.reply({
             content: 'There was an error while executing this command!',
