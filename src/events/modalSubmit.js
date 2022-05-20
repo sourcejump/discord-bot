4b4a1a0953e55dd0ddb53a724e20eabb1004317d
zeltim1 <tim.guenzel5@web.de>
2022-05-20 17:48:52 +0200

    added modalSubmit event

--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -3,18 +3,18 @@ const { MessageEmbed, Message } = require('discord.js');
 
 function createGiveawayEmbed(description, timelimit, emoji, image, winner) {
     let standardEmbedFields = [{
-        name: "🎁 Item",
+        name: '🎁 Item',
         value: description
     },
     {
-        name: "➡️ How to enter",
-        value: "React to this message with " + emoji
+        name: '➡️ How to enter',
+        value: 'React to this message with ' + emoji
     },
     {
-        name: "🏆 Winners",
-        value: "Winners are picked at random, and prizes are sent automatically through a direct message here on Discord when the giveaway concludes. Make sure you allow direct messages from other members!"
+        name: '🏆 Winners',
+        value: 'Winners are picked at random, and prizes are sent automatically through a direct message here on Discord when the giveaway concludes. Make sure you allow direct messages from other members!'
     }]
-    if (winner != "") {
+    if (winner != '') {
         let embed = new MessageEmbed()
             .setColor('GREEN')
             .addFields(
@@ -22,8 +22,8 @@ function createGiveawayEmbed(description, timelimit, emoji, image, winner) {
                 standardEmbedFields[1],
                 standardEmbedFields[2],
                 {
-                    name: "🏁 Giveaway ended",
-                    value: "Winner: " + winner
+                    name: '🏁 Giveaway ended',
+                    value: 'Winner: ' + winner
                 }
             )
             .setImage(image)
@@ -38,24 +38,26 @@ function createGiveawayEmbed(description, timelimit, emoji, image, winner) {
                 standardEmbedFields[2]
             )
             .setImage(image)
-            .setFooter({text:`Ending in ${Math.ceil(timelimit/60)} ${Math.ceil(timelimit/60) < 2?"hour":"hours"}`});
+            .setFooter({text:`Ending in ${Math.ceil(timelimit/60)} ${Math.ceil(timelimit/60) < 2?'hour':'hours'}`});
     return embed;
 }
 
 module.exports = {
-    name: "modalSubmit",
+    name: 'modalSubmit',
     async execute (modal) {
         try {
             if (modal.customId == 'giveaway') {
-            
-                //const channel = '838711380076396574';
-                const channel = '975791268741406800';
+                const channel = '838711380076396574';
                 const gamekey = modal.getTextInputValue('gamekey');
                 const description = modal.getTextInputValue('description');
                 let timelimit = modal.getTextInputValue('timelimit');
-                const emoji = `${modal.client.emojis.cache.find(emoji => emoji.name == modal.getTextInputValue("emoji"))}`;
+                if (isNaN(timelimit)) {
+                    await modal.reply({content:'Timelimit has to be integer.', ephemeral: true});
+                    return;
+                }
+                const emoji = `${modal.client.emojis.cache.find(emoji => emoji.name == modal.getTextInputValue('emoji'))}`;
                 if (emoji == 'undefined') {
-                    await modal.reply({content:"Emoji not found. Only custom server-emojis work.", ephemeral:true});
+                    await modal.reply({content:'Emoji not found. Only custom server-emojis work.', ephemeral:true});
                     return;
                 }
                 let image = '';
@@ -63,20 +65,20 @@ module.exports = {
                     image = modal.getTextInputValue('image');
                 }
     
-                let embed = createGiveawayEmbed(description,timelimit,emoji,image,"");
+                let embed = createGiveawayEmbed(description,timelimit,emoji,image,'');
     
                 let msg = await modal.client.channels.cache.get(channel).send({embeds: [embed]});
                 msg.react(emoji);
                 
                 let interval = setInterval(() => {
                     timelimit -= 1;
-                    embed = createGiveawayEmbed(description,timelimit,emoji,image,"");
+                    embed = createGiveawayEmbed(description,timelimit,emoji,image,'');
                     msg.edit({embeds: [embed]});
                     if (timelimit < 1) {
                         clearInterval(interval);
                         let winner;
                         msg.reactions.cache.map(async (reaction) => {
-                            let emojiName = emoji.split(":")[1];
+                            let emojiName = emoji.split(':')[1];
                             if (reaction.emoji.name !== emojiName) return;
                             let usersThatReacted = [];
                             let reactedUsers = await reaction.users.fetch();
@@ -86,7 +88,7 @@ module.exports = {
                                 }
                             });
                             if (usersThatReacted.length == 0) {
-                                winner = "No one reacted! :("
+                                winner = 'No one reacted! :('
                             } else {
                                 winner = usersThatReacted[Math.floor(Math.random()*usersThatReacted.length)];
                                 winner.send(`Congratulations! You won the giveaway for ${description}. \nYour Game key: ${gamekey}`);
@@ -98,7 +100,7 @@ module.exports = {
                         
                     }
                 }, 60000);
-                await modal.reply({content: "Done!", ephemeral: true});
+                await modal.reply({content: 'Done!', ephemeral: true});
             };
         } catch (e) {
             console.log(e);
