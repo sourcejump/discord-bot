ee11f33f1dcdd6ac50269286d745ba5731e7d116
zeltim1 <105654247+zeltim1@users.noreply.github.com>
2022-05-28 15:52:28 +0200

    moved interactionCreate back to index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
+const { modalSubmitGiveaway } = require('./src/events/giveaway.js');
 
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
@@ -58,6 +59,34 @@ const rest = new REST({ version: '9' }).setToken(token);
         console.error(err);
     }
 })();
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isModalSubmit()) {
+        if (interaction.customId == 'giveaway') {
+            modalSubmitGiveaway(interaction);
+        }
+    }
+
+    if (!interaction.isCommand()) {
+        return;
+    }
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true,
+        });
+        console.error(error);
+    }
+});
 
 client.once('ready', () => {
     console.log('Ready!');
